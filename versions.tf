4bf24682b098a8122b4f31e3b57498bed0dfc0f8
Roger Coll <roger.coll@global.ntt>
2021-07-02 11:23:40 +0200

    Upgrade Terraform and aws/hashicorp versions
    Terraform released the first stable version 1.0.0

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.12.26"
+  required_version = ">= 1.0.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.49"
+      version = ">= 3.47"
     }
     random = {
       source  = "hashicorp/random"
